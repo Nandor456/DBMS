625c2c4adc3a58e79737c165652b683fa97d3ee7
Nandor456 <mezeinandor23@gmail.com>
2025-03-25 17:24:14 +0200

    side-bar demo

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,14 +1,18 @@
 import DatabaseName from "./databaseName";
 import CollectionName from "./collectionName";
 import TableList from "./collectionTables";
+import { Fragment } from "react";
+import NestedList from "./NestedList";
+
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <NestedList></NestedList>
       <h1><DatabaseName></DatabaseName></h1>
       <h1><CollectionName></CollectionName></h1>
       <h1><TableList></TableList></h1>
-    </div>
+    </Fragment>
   );
 }
 

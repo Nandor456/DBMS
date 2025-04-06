231ef34fca7a97d3ba737e59a3791f8beaee8c18
Nandor456 <mezeinandor23@gmail.com>
2025-04-06 12:58:34 +0300

    merge

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,8 @@
 import { Drawer, CssBaseline, Toolbar, Box } from "@mui/material";
 import { useState } from "react";
 import DatabaseName from "./databaseName";
-import CollectionName from "./collectionName";
-import TableList from "./collectionTables";
+import CollectionName from "./CollectionName";
+import TableList from "./CollectionTable";
 import NestedList from "./NestedList";
 
 const drawerWidth = 250;
@@ -18,31 +18,35 @@ function App() {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: "border-box", backgroundColor:'#818181', borderRight: "5px solid black" }
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "#818181",
+            borderRight: "5px solid black",
+          },
         }}
       >
         <Toolbar />
-        <NestedList 
-          updateTrigger={updateTrigger} 
+        <NestedList
+          updateTrigger={updateTrigger}
           updateTriggerDB={updateTriggerDB}
-        /> 
+        />
       </Drawer>
 
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-        <h1><DatabaseName 
-          onDBCreated={() => setUpdateTriggerDB(prev => prev + 1)}
+        <h1>
+          <DatabaseName
+            onDBCreated={() => setUpdateTriggerDB((prev) => prev + 1)}
           />
         </h1>
         <h1>
-          <CollectionName  
-            onTableCreated={() => setUpdateTrigger(prev => prev + 1)} 
+          <CollectionName
+            onTableCreated={() => setUpdateTrigger((prev) => prev + 1)}
           />
-        </h1> 
+        </h1>
         <h1>
-          <TableList 
-            updateTrigger={updateTrigger} 
-          />
+          <TableList updateTrigger={updateTrigger} />
         </h1>
       </Box>
     </Box>

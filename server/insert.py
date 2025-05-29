d7f4ab35219f288d0229f04af7a9f3b958f10bcd
vegh <veghdaniel4@gmail.com>
2025-05-29 17:17:30 +0300

    dani old

--- server/insert.py
+++ server/insert.py
@@ -1,12 +1,15 @@
 import requests
-
+import random
+from faker import Faker
 #url = 'http://localhost:4000/database/row/insert'
 url = 'http://localhost:4000/database/row/insert'
-names = ['John', 'Mike', 'Claudia', 'Wilson', 'Kevin', 'Potter']
-ages = ['21', '23', '19', '18', '21', '21']
-for id in range(6):
+names_list = ['John', 'Mike', 'Claudia', 'Wilson', 'Kevin', 'Potter', 'Alice', 'Bob', 'Emma', 'Liam']
+fake = Faker()
+for id in range(47162,100007,1):
+    names = fake.name()
+    ages = random.randint(18, 25)
     data = {
-        'query': f"Use school;\ninsert into student('id', 'name', 'age') values({id}, {names[id]}, {ages[id]});"
+        'query': f"Use school;\ninsert into student('id', 'name', 'age') values({id}, {names}, {ages});"
     }
     id = id + 1
     response = requests.post(url, json=data)
@@ -15,6 +18,5 @@ for id in range(6):
     if response.ok:
         try:
             result = response.json()
-            print(result)
         except ValueError:
             print(f"ID {id}: Failed to parse JSON. Raw response: {response}")
